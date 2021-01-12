dba2395815da3c5cc96d9149f8a187314bd3a361
Marat Fakhrutdinov <maratfakhrutdinov2019@gmail.com>
2021-01-13 00:40:53 +0300

    add agreement main form js

---
+++ JS/Forms/autodeal_agreement_main_form.js
@@ -0,0 +1,77 @@
+var AutoDealer = AutoDealer || {};
+
+AutoDealer.autodeal_agreement = (function () {
+    let hideTabs = function (ctx) {
+        let formContext = ctx.getFormContext();
+
+        formContext.ui.tabs.get("credit_tab").setVisible(false);
+        //formContext.ui.tabs.get("payment_tab").setVisible(false);
+    };
+
+    let disableCreditTabFields = function (ctx) {
+        let formContext = ctx.getFormContext();
+
+        let creditTab = formContext.ui.tabs.get("credit_tab");
+
+        //hide credit tab fields except autodeal_creditid
+        creditTab.sections.forEach(section => {
+            section.controls.forEach(control => {
+                if (control.getName() !== "autodeal_creditid") {
+                    control.setDisabled(true);
+                }
+            })
+        });
+    }
+
+    let enableCreditTabFields = function (ctx) {
+        let creditTab = ctx.getFormContext().ui.tabs.get("credit_tab");
+
+        creditTab.sections.forEach(section => {
+            section.controls.forEach(control => {
+                control.setDisabled(false);
+            })
+        });
+    }
+
+    let showCreditTab = function (ctx) {
+        let formContext = ctx.getFormContext();
+        let contact = formContext.getAttribute("autodeal_contact").getValue();
+        let automobile = formContext.getAttribute("autodeal_autoid").getValue();
+
+        if (contact[0].id && automobile[0].id) {
+            formContext.ui.tabs.get("credit_tab").setVisible(true);
+        }
+    }
+
+    let clearNumber = function (ctx) {
+        let formContext = ctx.getFormContext();
+        let numberAttr = formContext.getAttribute("autodeal_name");
+        let numberStr = numberAttr.getValue();
+        let cleared = "";
+
+        for (let c of numberStr) {
+            if (c >= "0" && c <= "9" || c === "-") {
+                cleared += c;
+            }
+        }
+
+        numberAttr.setValue(cleared);
+    }
+
+    let addEventHandlers = function (ctx) {
+        let formContext = ctx.getFormContext();
+
+        formContext.getAttribute("autodeal_contact").addOnChange(showCreditTab);
+        formContext.getAttribute("autodeal_autoid").addOnChange(showCreditTab);
+        formContext.getAttribute("autodeal_creditid").addOnChange(enableCreditTabFields);
+        formContext.getAttribute("autodeal_name").addOnChange(clearNumber);
+    };
+
+    return {
+        onLoad: function (ctx) {
+            hideTabs(ctx);
+            disableCreditTabFields(ctx);
+            addEventHandlers(ctx);
+        }
+    };
+})();
